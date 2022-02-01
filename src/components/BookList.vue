b0bda0987821187c5054470129e7323593b7f186
Joakim Vanström <joakim.vanstrom@gmail.com>
2022-02-01 18:53:14 +0100

    Initial commit

---
+++ src/components/BookList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <BookCover v-for="book in books" :key="book.Title" :book="book"/>
+  </div>
+</template>
+
+<script>
+
+import BookCover from './BookCover.vue'
+export default {
+    components: {BookCover},
+    props: ['books']
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id="wrapper">
+    <h1>Classic Childrens books</h1>
+    <BookList class="all-books" :books="books"/>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import books from '../assets/books.json'
+import BookList from '../components/BookList.vue'
+export default {
+  components: {BookList},
+  data(){return{
+    books: [...books]
+}}
+}
+</script>
+
+
+<style lang="scss">
+#wrapper {
+  padding-top: 4rem;
+  h1 {
+    padding-left: 1rem;
+  }
+}
+.all-books{
+  padding-top: 3rem;
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2,1fr);
+  grid-gap: 1rem;
+
+}
+
+</style>
